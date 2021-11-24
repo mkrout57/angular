34059eb15205e50c0b7eeb34255c7d1e70ee36a3
MUKESH <mukesh.rout@oroprise.com>
2021-11-24 23:34:58 +0530

    Assignment One

---
+++ app.js
@@ -0,0 +1,23 @@
+(function(){
+    'use strict';
+
+    angular.module('LunchCheck',[])
+    .controller('LunchCheckController', function($scope){
+        $scope.lunchItem = '';
+        $scope.checkData = function (){
+            var menu = $scope.lunchItem;
+            var output = '';
+            if(menu == '')
+                output = 'Please enter data first';
+            else{    
+                var totalItem = menu.split(',').length;
+            
+                if(totalItem <=3 )
+                    output = 'Enjoy!';
+                else if(totalItem > 3 )
+                    output = 'Too much!';
+            }
+            $scope.textValue = output;    
+        };
+    });
+})();
